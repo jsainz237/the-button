ba03cffe2df366652ee6c0fc9c53afb84380ea0f
Jesse <jsainz237@gmail.com>
2020-05-11 00:50:28 -0500

    add ng-bootstrap to project

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.login(this.username, this.password)
+      .subscribe(data => console.log(data));
   }
 
+  submit() {
+    console.log(this.username);
+  }
 }
