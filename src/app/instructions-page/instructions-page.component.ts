4c3965f3066700f1731e5e8b35a78a367b2e0f97
Jesse <jsainz237@gmail.com>
2020-05-21 23:54:36 -0500

    create instructions component/page

---
+++ src/app/instructions-page/instructions-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions-page',
+  templateUrl: './instructions-page.component.html',
+  styleUrls: ['./instructions-page.component.scss']
+})
+export class InstructionsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
